153ca29fbfc6b68ddda3c1558cb3d1c4d292f1ee
Sawyer <sawyerb@ksu.edu>
2025-08-11 18:04:44 -0500

    feat: add screen reader text styles and improve external link icon handling to remove duplicate links

--- styles/tiny-framework/js/functions.js
+++ styles/tiny-framework/js/functions.js
@@ -12,7 +12,22 @@
 	 *
 	 * Tip25 - Mark the links that will open in a new window with special icon, usually these are the links to external resources.
 	 */
-	$( '.entry-content a[target="_blank"]' ).append( '<span class="icon-webfont fa-external-link" aria-hidden="true"></span><span class="screen-reader-text">' + tinyframeworkAdditionalScripts.newWindow + '</span>' );
+	$( '.entry-content a[target="_blank"]' ).each( function() {
+		var $a = $( this );
+		// Skip if disabled on this link
+		if ( $a.hasClass( 'no-link-icon' ) ) {
+			return;
+		}
+		var hasIcon = $a.find( '.fa-external-link' ).length > 0 || $a.hasClass( 'icon-webfont' ) || $a.find( '.icon-webfont' ).length > 0;
+		var hasSr   = $a.find( '.screen-reader-text' ).length > 0;
+		if ( ! hasIcon ) {
+			$a.append( '<span class="icon-webfont fa-external-link" aria-hidden="true"></span>' );
+		}
+		if ( ! hasSr ) {
+			var srText = ( window.tinyframeworkAdditionalScripts && tinyframeworkAdditionalScripts.newWindow ) ? tinyframeworkAdditionalScripts.newWindow : 'Opens in a new window';
+			$a.append( '<span class="screen-reader-text">' + srText + '</span>' );
+		}
+	} );
 
 	// Remove title attributes when the title attribute is the same as the link text
     $('*').each( function () {
